2c15005ccd6856eee04539ef51cf57c651d1ba4c
DarinaSanna <63007898+DarinaSanna@users.noreply.github.com>
2020-04-07 22:17:39 +0300

    Methods created

--- Arrays.java
+++ Arrays.java
@@ -6,26 +6,39 @@ public class Arrays {
         int numbersCount = numbers.length;
         Random random = new Random();
 
-        for (int i = 0; i < numbersCount-1; i++) {
+        fillArray(numbers, numbersCount, random);
+
+        sort(numbers, numbersCount);
+
+        printArray(numbers, numbersCount);
+
+    }
+
+    private static void fillArray(int[] numbers, int numbersCount, Random random) {
+        for (int i = 0; i < numbersCount; i++) {
             numbers [i] = random.nextInt(100);
         }
+    }
 
+    private static void sort(int[] numbers, int numbersCount) {
         boolean sorted = false;
         while (!sorted) {
             sorted = true;
-            for (int i = 0; i < numbersCount-1; i++) {
-                if (numbers[i] > numbers[i+1]){
+            for (int i = 0; i < numbersCount - 1; i++) {
+                if (numbers[i] > numbers[i + 1]) {
                     int x = numbers[i];
-                    numbers[i] = numbers[i+1];
-                    numbers[i+1] = x;
+                    numbers[i] = numbers[i + 1];
+                    numbers[i + 1] = x;
                     sorted = false;
+                    numbersCount = numbersCount-1;
                 }
-
             }
         }
+    }
+
+    private static void printArray(int[] numbers, int numbersCount) {
         for (int i = 0; i < numbersCount; i++) {
             System.out.println(numbers[i]);
         }
     }
-
 }
